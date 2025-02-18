4694ed4dd1203d9159a5baa2bc59d55c697f6e68
Ajay Barman <barmanjiaj@gmail.com>
2025-02-18 22:06:32 +0530

    update

--- nvim/.config/nvim/lua/Barmanji/lazy/copilot.lua
+++
@@ -1,5 +0,0 @@
--- git clone https://github.com/github/copilot.vim.git  ~/.config/nvim/pack/github/start/copilot.vim
--- also you need to login to github with your username and password, so that you can use the copilot plugin.
-return {
-  "github/copilot.vim"
-}
