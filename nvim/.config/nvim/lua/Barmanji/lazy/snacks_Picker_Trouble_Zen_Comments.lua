011f26dbf06c8b1b2cf3720da9533db9801066e7
Ajay Barman <barmanjiaj@gmail.com>
2025-02-23 21:42:33 +0530

    minor fix, dressing.lua scope fixed

--- nvim/.config/nvim/lua/Barmanji/lazy/snacks_Picker_Trouble_Zen_Comments.lua
+++ nvim/.config/nvim/lua/Barmanji/lazy/snacks_Picker_Trouble_Zen_Comments.lua
@@ -128,7 +128,7 @@ return {
                             },
                         },
                     },
-                }
+                },
             },
             dashboard = { -- WHY DO I HAVE THIS BULLSHIP? Becuase i love ship just dont want to ride inside em :_)
                 -- {THIS SUCKS}
@@ -142,15 +142,21 @@ return {
                 },
             },
         },
+        -- HACK: LEARN EM:
+        -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        -- vim.keymap.set("n", "gD", function() vim.lsp.buf.peek_definition() end, opts) -- Opens in a floating window if supported
+        -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
         -- NOTE: Keymaps
         keys = {
-
-            { "<leader>lg",       function() require("snacks").lazygit() end,                                        desc = "Lazygit" },
-            { "<leader>gl",       function() require("snacks").lazygit.log() end,                                    desc = "Lazygit Logs" },
-            { "<leader>es",       function() require("snacks").explorer() end,                                       desc = "Open Snacks Explorer" },
-            { "<leader>rN",       function() require("snacks").rename.rename_file() end,                             desc = "Fast Rename Current File" },
-            { "<leader>dB",       function() require("snacks").bufdelete() end,                                      desc = "Delete or Close Buffer  (Confirm)" },
-
             -- Snacks Picker
             { "<leader>pf",       function() require("snacks").picker.files() end,                                   desc = "Find Files (Snacks Picker)" },
             { "<leader>pn",       function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Nvim Config File" },
@@ -160,19 +166,25 @@ return {
             { "<leader>ps",       function() require("snacks").picker.smart() end,                                   desc = "Smart Find Files" },
             { "<leader><leader>", function() require("snacks").picker.buffers() end,                                 desc = "Find existing buffers" },
             { "<leader>pr",       function() require("snacks").picker.recent() end,                                  desc = "Recent" },
+            { "<leader>pq",       function() require("snacks").picker.qflist() end,                                  desc = "Quickfix List" },
+            { "<leader>pd",       function() require("snacks").picker.diagnostics() end,                             desc = "Diagnostics" },
+            { "<leader>pD",       function() require("snacks").picker.diagnostics_buffer() end,                      desc = "Buffer Diagnostics" },
 
             -- Git Stuff
+            { "<leader>Gl",       function() require("snacks").lazygit.log() end,                                    desc = "Lazygit Logs" },
+            { "<leader>lg",       function() require("snacks").lazygit() end,                                        desc = "Lazygit" },
             { "<leader>gbr",      function() require("snacks").picker.git_branches({ layout = "select" }) end,       desc = "Git Branches" },
             { "<leader>gl",       function() require("snacks").picker.git_log() end,                                 desc = "Git Log" },
             { "<leader>gL",       function() require("snacks").picker.git_log_line() end,                            desc = "Git Log Line" },
             { "<leader>GS",       function() require("snacks").picker.git_status() end,                              desc = "Git Status" },
             { "<leader>Gs",       function() require("snacks").picker.git_stash() end,                               desc = "Git Stash" },
+
             -- Other Utils
-            { "<leader>gd",       function() require("snacks").picker.lsp_definitions() end,                         desc = "Goto Definition" },         -- already in init.lua
+            { "<leader>es",       function() require("snacks").explorer() end,                                       desc = "Open Snacks Explorer" },
+            { "<leader>rN",       function() require("snacks").rename.rename_file() end,                             desc = "Fast Rename Current File" },
+            { "<leader>dB",       function() require("snacks").bufdelete() end,                                      desc = "Delete or Close Buffer  (Confirm)" },
+            { "<leader>gd",       function() require("snacks").picker.lsp_definitions() end,                         desc = "Goto Definition" }, -- already in init.lua
             { "<leader>gr",       function() require("snacks").picker.lsp_references() end,                          nowait = true,                             desc = "References" },
-            { "<leader>sq",       function() require("snacks").picker.qflist() end,                                  desc = "Quickfix List" },
-            { "<leader>sd",       function() require("snacks").picker.diagnostics() end,                             desc = "Diagnostics" },
-            { "<leader>sD",       function() require("snacks").picker.diagnostics_buffer() end,                      desc = "Buffer Diagnostics" },
             { "<leader>th",       function() require("snacks").picker.colorschemes({ layout = "ivy" }) end,          desc = "Pick Color Schemes" },
             { "<leader>vh",       function() require("snacks").picker.help() end,                                    desc = "Help Pages" },
         }
@@ -224,8 +236,6 @@ return {
                 vim.wo.rnu = true
                 ColorMyPencils()
             end)
-
-
             vim.keymap.set("n", "<leader>zZ", function()
                 require("zen-mode").setup {
                     window = {
