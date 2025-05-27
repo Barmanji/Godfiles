a6a5f2c69f5b221367e6a783c8a78087a6549939
Ajay Barman <barmanjiaj@gmail.com>
2025-05-27 20:40:19 +0530

    life updates

--- hypr/.config/hypr/scripts/ChangeBlur.sh
+++ hypr/.config/hypr/scripts/ChangeBlur.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # Script for changing blurs on the fly
 
 notif="$HOME/.config/swaync/images/bell.png"
 
-STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
+STATE=$(hyprctl -j getoption decoration:blur:enabled | jq ".int")
 
 if [ "${STATE}" == "0" ]; then
-	hyprctl keyword decoration:blur:size 4
-	hyprctl keyword decoration:blur:passes 4
- 	notify-send -e -u critical -i "$notif" "Blur"
+    hyprctl keyword decoration:blur:enabled true
+    notify-send -e -u critical -i "$notif" "Blur"
 else
-	hyprctl keyword decoration:blur:size 0
-	hyprctl keyword decoration:blur:passes 0
-  	notify-send -e -u critical -i "$notif" "Transparent"
+    hyprctl keyword decoration:blur:enabled false
+    notify-send -e -u critical -i "$notif" "Transparent"
 fi
