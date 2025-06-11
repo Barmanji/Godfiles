de961715b8a3396cc1e12fb232faeed97c2a3d58
Ajay Barman <barmanjiaj@gmail.com>
2025-06-11 09:23:12 +0530

    feat(life updates): nightLight script addd, tinkered waybar

--- scripts/scripts/nightLight_brightness.sh
+++ scripts/scripts/nightLight_brightness.sh
@@ -5,15 +5,18 @@
 # export WAYLAND_DISPLAY="wayland-1"
 # export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
+notif="$HOME/.config/swaync/images/bell.png"
 currTime=$(date +%H)
 
-if [[ "$currTime" -ge 4 ]] || [[ "$currTime" -le 21 ]]; then
-    echo "Hi"
+if [[ "$currTime" -ge 4 ]] && [[ "$currTime" -le 21 ]]; then
+    echo "hi"
     hyprctl hyprsunset gamma 100
     hyprctl hyprsunset temperature 6000
+    notify-send -e -u critical -i "$notif" "NightLight Off"
 else
-    hyprctl hyprsunset temperature 5500
-    hyprctl hyprsunset gamma 97
+    hyprctl hyprsunset temperature 5000
+    hyprctl hyprsunset gamma 90
+    notify-send -e -u critical -i "$notif" "NightLight On"
 fi
 
 
