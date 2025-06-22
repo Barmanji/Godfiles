b032ee92bca4600a93111dc5486a414eee0f3463
Ajay Barman <barmanjiaj@gmail.com>
2025-06-22 20:29:25 +0530

    update hyprsunset

--- hypr/.config/hypr/scripts/Hyprsunset_Auto_NightLight8PM.sh
+++ hypr/.config/hypr/scripts/Hyprsunset_Auto_NightLight8PM.sh
@@ -9,10 +9,6 @@ export WAYLAND_DISPLAY="wayland-1"
 export XDG_RUNTIME_DIR="/run/user/1000"
 export HYPRLAND_INSTANCE_SIGNATURE="$hyprlandSession"
 
-# hyprctl hyprsunset temperature 4500
-# hyprctl hyprsunset gamma 90
-# notify-send -e -u critical -i "$notif" "NightLight On"
-
 if [[ "$currTime" -ge 4 ]] && [[ "$currTime" -lt 20 ]]; then
     hyprctl hyprsunset gamma 100
     hyprctl hyprsunset temperature 6000
